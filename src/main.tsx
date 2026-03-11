99c231a795b3b1254ea14902f850eac0653b183b
Meghdad Jafari <megberserker@gmail.com>
2026-03-12 00:59:43 +0100

    Add routing, placeholder pages and navbar with Google auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
+import HomePage from './pages/HomePage.tsx'
+import ProfilePage from './pages/ProfilePage.tsx'
+import SavedJobsPage from './pages/SavedJobsPage.tsx'
+import ApplicationsPage from './pages/ApplicationsPage.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="profil" element={<ProfilePage />} />
+          <Route path="sparade" element={<SavedJobsPage />} />
+          <Route path="ansokningar" element={<ApplicationsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
